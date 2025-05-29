63a075819e213cc2207ed29d30b6e5b6adbc9693
Paranat Klinhom <paranat.klinhom@MacBook-Pro-khxng-Admin.local>
2025-05-29 16:54:46 +0700

    ✨ (Head.tsx): add structured data in JSON-LD format for better SEO and     representation of the organization 📝 (layout.tsx): update keywords in metadata to enhance search visibility     and relevance for the content provided

--- src/app/Head.tsx
+++ src/app/Head.tsx
@@ -2,12 +2,35 @@
 export default function Head() {
   return (
     <>
-      <></>
-      {/* <link
-        rel="stylesheet"
-        href={`https://api.playground.chidahp.com/wp-includes/css/dist/block-library/style.min.css?v=${Date.now()}`}
-        precedence="default"
-      /> */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "MediaOrganization",
+            "name": "โรงเรียนชูโล่วิทยาคม",
+            "description": "Playground คือสนามทดลองข่าว บทความ Podcast และเรื่องซน ๆ จากจักรวาลชี้ดาบ - จัดทำโดย นักเรียนชูโล่วิทยาคม",
+            "alternativeName": "Playground by Chidahp",
+            "url": "https://playground.chidahp.com",
+            "sameAs": [
+              "https://www.instagram.com/chidahp",
+              "https://www.youtube.com/@chidahp",
+              "https://www.facebook.com/CHIDAHP"
+            ],
+            "logo": {
+              "@type": "ImageObject",
+              "url": "https://playground.chidahp.com/chidahp.png",
+              "width": 512,
+              "height": 512
+            },
+            "parentOrganization": {
+              "@type": "Organization",
+              "name": "สำนักพิมพ์ชี้ดาบ",
+              "url": "https://www.chidahp.com"
+            }
+          })
+        }}
+      />
     </>
   );
 }
