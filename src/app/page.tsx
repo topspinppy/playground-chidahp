fc586a68ff7a5e03f4b52f8ed52f843ffd28924a
topspinppy <tl.topspin.48@gmail.com>
2025-08-27 11:34:55 +0700

    fix: remove south dakota

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { getFeaturedPost, getLatestPosts, getNotLatestPosts, getPostSeries } fro
 import RoadTo100K from "./components/front/RoadTo100K";
 import LatestPosts from "./components/front/LatestPosts";
 import SeriesSection from "./components/front/SeriesSection";
-import SouthDakotaBanner from "./components/front/SouthDakotaBanner";
 
 export const dynamic = 'force-dynamic';
 
@@ -16,7 +15,7 @@ export default async function HomePage() {
 
   return (
     <main className="max-w-6xl mx-auto px-4 py-10 space-y-16 mb-0">
-      <SouthDakotaBanner />
+      {/* <SouthDakotaBanner /> */}
       <FeaturedPost featuredPost={featuredPost} latestPosts={latestPosts} />
       <LatestPosts latestPosts={notLatestPosts} />
       <SeriesSection postSeries={postSeries} />
